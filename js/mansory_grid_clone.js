bdf5ab36bdc1e627944523dd5a62c53a9433c457
César Aparicio <clark012389@hotmail.com>
2019-12-27 21:38:36 -0600

    Implementacion de grid, añadiendo comentarios

---
+++ js/mansory_grid_clone.js
@@ -0,0 +1,23 @@
+(function() {
+  window.addEventListener("load", resize); //Escucha la carga de la pagina
+  window.addEventListener("resize", resize); // Escucha el redimencionamiento
+
+  function resize() {
+    const grid = document.querySelector(".grid"); // Elemento grid (rejilla)
+    const rowHeight = getStyleValue(grid, "grid-auto-rows"); // numero de filas en el grid
+    const rowGap = getStyleValue(grid, "grid-row-gap"); // espacio entre las filas
+    grid.style.gridAutoRows = "auto";
+    grid.style.alignItems = "self-start"; // Para alinearlos apilados
+    //A todos los elementos del grid, asignales una altura con span, deacuerdo a su contenido
+    grid.querySelectorAll(".item").forEach(item => {
+      item.style.gridRowEnd = `span ${Math.ceil(
+        (item.clientHeight + rowGap) / (rowHeight + rowGap)
+      )}`;
+    });
+    grid.removeAttribute("style"); //Remueve el atributo estilo para que al redimensionar vuelva a ponerselo
+  }
+ 
+  function getStyleValue(element, style) {
+    return parseInt(window.getComputedStyle(element).getPropertyValue(style));
+  }
+})();
